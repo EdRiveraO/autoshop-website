82b56712438ed8fb0264708887d9c934fd9de726
EdRiveraO <edwinrivera1810@gmail.com>
2022-01-11 16:02:42 -0500

    container

--- script/script.js
+++ script/script.js
@@ -1,5 +1,19 @@
 let menuBtn = document.querySelector('#menu-btn');
 let navbar = document.querySelector('.header .navbar');
+let contenPr = document.querySelector('.principal');
+let Slides = document.querySelectorAll('.slide');
+
+contenPr.onmouseover=()=>{ 
+   Slides[0].style.opacity= "1";
+   Slides[1].style.opacity= "1";
+} 
+contenPr.onmouseout=()=>{ 
+   Slides[0].style.opacity= "0";
+   Slides[1].style.opacity= "0";
+} 
+
+
+
 
 menuBtn.onclick = () =>{
    menuBtn.classList.toggle('fa-times');
